5582bf7d811bd555cdd905474c5a084d01a29754
felixhertrampf <hertrampf.f@gmail.com>
2020-05-25 17:01:17 +0200

    The Factory was way too specific and the reducer wasn't extendable. This new Factory is much more customizable.

--- EntityContextFactory.tsx
+++ EntityContextFactory.tsx
@@ -1,64 +1,30 @@
-import Entity from "../Entity";
-import React, {Component, Context, createContext, Dispatch, useReducer} from "react";
+import Entity from "Entity";
+import React, {Component, Context, createContext, Dispatch, Reducer, useReducer} from "react";
 
-export enum CUDReducerActions {
-    CREATE = "CREATE",
-    UPDATE = "UPDATE",
-    DELETE = "DELETE"
+export interface EntityDispatch<A> {
+    dispatch: Dispatch<A>
 }
 
-export enum RestReducerActions {
-    REFRESH = "REFRESH"
-}
+export default class EntityContextFactory<T extends Entity, S, A> extends Component {
+    private readonly entityStateContext: Context<S>;
+    private readonly entityDispatchContext: Context<EntityDispatch<A>>;
+    private readonly entityReducer: Reducer<S, A>;
+    private readonly initialState: S;
 
-interface EntityState<T> {
-    entities: T[]
-}
-
-type Action<T> = {
-    action: CUDReducerActions
-    payload: T
-} | {
-    action: RestReducerActions,
-    payload: T[]
-}
-
-
-export interface EntityDispatch<T> {
-    dispatch: Dispatch<Action<T>>
-}
-
-
-export default class EntityContextFactory<T extends Entity> extends Component {
-    private readonly entityStateContext: Context<EntityState<T>>;
-    private readonly entityDispatchContext: Context<EntityDispatch<T>>;
-
-    constructor(props: any = {}, initialState?: EntityState<T>) {
+    constructor(props: any = {}, initialState: S, reducer: Reducer<S, A>) {
         super(props);
-        this.entityStateContext = createContext<EntityState<T>>(initialState || {entities: []});
-        this.entityDispatchContext = createContext<EntityDispatch<T>>({dispatch: () => null});
+        this.initialState = initialState;
+        this.entityReducer = reducer;
+        this.entityStateContext = createContext<S>(initialState);
+        this.entityDispatchContext = createContext<EntityDispatch<A>>({dispatch: () => null});
     }
 
-    entityReducer = (state: EntityState<T>, action: Action<T>): EntityState<T> => {
-        switch (action.action) {
-            case CUDReducerActions.CREATE:
-                return {entities: [...state.entities, action.payload]};
-            case CUDReducerActions.DELETE:
-                return {entities: state.entities.filter(value => value.id !== action.payload.id)};
-            case CUDReducerActions.UPDATE:
-                return {entities: state.entities.map(value => value.id === action.payload.id ? action.payload : value)};
-
-            case RestReducerActions.REFRESH:
-                return {entities: action.payload};
-        }
-    };
-
-    createAll = (): [Context<EntityState<T>>, Context<EntityDispatch<T>>, (props: { children: any }) => JSX.Element] => {
+    createAll = (): [Context<S>, Context<EntityDispatch<A>>, (props: { children: any }) => JSX.Element] => {
         return [this.entityStateContext, this.entityDispatchContext, this.entityContextProvider]
     };
 
     entityContextProvider = (props: { children: any }) => {
-        const [entityState, entityDispatch] = useReducer(this.entityReducer, {entities: []});
+        const [entityState, entityDispatch] = useReducer(this.entityReducer, this.initialState);
         return (
             <this.entityStateContext.Provider value={entityState}>
                 <this.entityDispatchContext.Provider value={{dispatch: entityDispatch}}>
@@ -68,3 +34,4 @@ export default class EntityContextFactory<T extends Entity> extends Component {
         )
     }
 }
+
